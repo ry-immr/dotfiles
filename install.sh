8cd55f9e677a302f1f4037321c0bca3b11cd37ba
ry-immr <42567842+ry-immr@users.noreply.github.com>
2020-09-03 00:45:31 +0900

    Change to display error message when the target directory does not exist

--- install.sh
+++ install.sh
@@ -4,12 +4,15 @@ DOTFILES_PATH="$HOME/dotfiles"
 
 git clone git@github.com:ry-immr/dotfiles.git "$DOTFILES_PATH"
 
-cd $DOTPATH
+cd "$DOTFILES_PATH"
+if [ $? -ne 0 ]; then
+    die "not found: $DOTFILES_PATH"
+fi
 
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".DS_Store" ] && continue
 
-    ln -snfv "$f" "$HOME/$f"
+    ln -snfv "$DOTFILES_PATH/$f" "$HOME/$f"
 done
