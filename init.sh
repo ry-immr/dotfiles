b11fc9f785e0128f827b9227703a94afc9ac9aa0
ry-immr <42567842+ry-immr@users.noreply.github.com>
2020-09-19 04:07:29 +0900

    add installing and set global 3.7.9

--- init.sh
+++ init.sh
@@ -8,3 +8,7 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 exec "$SHELL"
+
+pyenv install 3.7.9
+pyenv global 3.7.9
+pyenv rehash
